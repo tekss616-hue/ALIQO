d7e4bb42869969204be96f8428e0bc900b82cd29
tekss616-hue <tekss616@gmail.com>
2026-09-04 08:37:56 +0300

    fix(ui): import dp for modern home layout

--- app/src/main/java/com/aliqo/app/ModernMainActivity.kt
+++ app/src/main/java/com/aliqo/app/ModernMainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import retrofit2.HttpException
